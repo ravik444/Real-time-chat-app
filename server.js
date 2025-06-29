2b4fa1ddefcd91bbc08a68d89942a61db81b347d
ravik444 <ravicseg2@gmail.com>
2025-06-29 22:52:33 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*', // allow frontend access
+    methods: ['GET', 'POST']
+  }
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('send_message', (data) => {
+    io.emit('receive_message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log('✅ Server running on http://localhost:3001');
+});
